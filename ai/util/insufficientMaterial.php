40f32e433a6769338fbaf9b49917e9d0ba4fed8a
Parker Harris <rparkerharris@gmail.com>
2024-01-01 17:37:18 -0800

    Fix king search bug

--- ai/util/insufficientMaterial.php
+++ ai/util/insufficientMaterial.php
@@ -4,7 +4,7 @@
         $minorPieces = 0;
         foreach ($gameData->grid as $row) {
             foreach ($row as $piece) {
-                if ($piece != "-") {
+                if ($piece != "-" && $piece != "k" && $piece != "K") {
                     // only keep track of bishops and knights - if any other pieces found, there is sufficient material
                     if (in_array($piece, ["n", "b", "N", "B"])) {
                         $minorPieces += 1;
